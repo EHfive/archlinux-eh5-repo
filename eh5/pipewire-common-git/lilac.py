864cc650947958d43222175302d3b83cd0f78f34
Huang-Huang Bao <eh5@sokka.cn>
2022-11-23 16:59:27 +0800

    pipewire-*-git: init

---
+++ eh5/pipewire-common-git/lilac.py
@@ -0,0 +1,29 @@
+import os
+from types import SimpleNamespace
+from lilaclib import *
+
+dirname = os.path.dirname(os.path.realpath(__file__))
+g = SimpleNamespace()
+
+build_prefix = 'extra-x86_64'
+
+
+def pre_build():
+    res = run_cmd([
+        'python',
+        os.path.join(dirname, '../pipewire-template/render.py'),
+        'common.json',
+        dirname
+    ])
+    g.files = res.splitlines()
+    vcs_update()
+
+
+def post_build():
+    git_add_files(g.files)
+    git_commit()
+    update_aur_repo()
+
+
+if __name__ == '__main__':
+    single_main()
